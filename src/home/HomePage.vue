642613ac8ce7213017af4245b8fbb3f68c87fd94
skyup259 <saurabh.kumar259@gmail.com>
2019-04-02 17:23:32 +0530

    loing and res

---
+++ src/home/HomePage.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h1>Hi {{account.user.firstName}}!</h1>
+        <em v-if="users.loading">Loading users...</em>
+        
+        <p>
+            <router-link to="/login">Logout</router-link>
+        </p>
+    </div>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex'
+
+export default {
+    computed: {
+        ...mapState({
+            account: state => state.account,
+            users: state => state.users.all
+        })
+    },
+    created () {
+        this.getAllUsers();
+    },
+    methods: {
+        ...mapActions('users', {
+            getAllUsers: 'getAll',
+            deleteUser: 'delete'
+        })
+    }
+};
+</script>
